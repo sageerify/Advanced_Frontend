a48ee53d7c545b5d3f7eef198b73bbcfda671b28
Sageer Ahmed <s.hunzai100@gmail.com>
2025-01-04 07:15:04 -0800

    Seperate Interview given student and nor given using array

---
+++ All About JavaScript/Array/Script.js
@@ -0,0 +1,16 @@
+const students = [
+    { name: "Sameer", interviewGiven: true },
+    { name: "Mushraf", interviewGiven: false },
+    { name: "Sameer", interviewGiven: true },
+    { name: "Ahson", interviewGiven: false },
+    { name: "Daniyal", interviewGiven: true },
+];
+
+//filter and map
+const notInterviewed = students.filter(s => !s.interviewGiven).map(s => ({ ...s, addedToList: true }));
+
+// Separate 
+const interviewed = students.filter(s => s.interviewGiven);
+
+console.log("Not Interviewed (Added):", notInterviewed);
+console.log("Interviewed:", interviewed);
