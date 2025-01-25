8a4d3fc69b24707a5edf88b1aa0b73c06bc699fc
Sageer Ahmed <160208153+sageerify@users.noreply.github.com>
2025-01-25 09:53:06 -0800

    Add files via upload

---
+++ All About JavaScript/script.js
@@ -0,0 +1,18 @@
+let x = 1;
+switch(x)
+ {
+
+    case 1:
+        console.log("hello");
+        break;
+    case 2:
+        console.log("hi");
+        break;
+    case 3:
+        console.log(bye);
+        break;
+
+        default:
+            console.log("ok")
+
+}
